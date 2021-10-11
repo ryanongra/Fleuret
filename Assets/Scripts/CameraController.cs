6a3d47fafc1f7a8506b4222027fffa80beee6933
ryanongra <ryanongra@gmail.com>
2021-10-11 22:13:54 +0800

    Initial Commit

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/**
+ * Credits to Learn Everything Fast for this script
+ * https://www.youtube.com/watch?v=lYIRm4QEqro
+ */
+public class CameraController : MonoBehaviour
+{
+
+    public float speedH = 2.0f;
+    public float speedV = 2.0f;
+
+    public float yaw = 0.0f;
+    public float pitch = 0.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        yaw += speedH * Input.GetAxis("Mouse X");
+        pitch -= speedV * Input.GetAxis("Mouse Y");
+
+        transform.eulerAngles = new Vector3(pitch, yaw, 0.0f);
+    }
+}
